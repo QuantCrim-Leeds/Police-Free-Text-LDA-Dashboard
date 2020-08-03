99985d4041baf8da600ecbff546593e536bc8668
sparrow0hawk <alexjcoleman@gmail.com>
2020-08-03 08:55:15 +0100

    attempt at cli_test but fails

--- topic_model_to_Shiny_app/tests/cli_test.py
+++ topic_model_to_Shiny_app/tests/cli_test.py
@@ -3,7 +3,7 @@ import unittest
 from unittest.mock import patch
 import pkg_resources
 from click.testing import CliRunner
-from topic2shiny import main
+from topic_model_to_Shiny_app.topic2shiny import main
 
 resource_package = 'topic_model_to_Shiny_app'
 
@@ -15,11 +15,19 @@ class CLI_test(unittest.TestCase):
         # these are examples and will fail
         # TODO: update these to actually test CLI
         runner = CliRunner()
-        result = runner.invoke(cli, ['--debug', 'sync'])
-        assert result.exit_code == 0
-        assert 'Debug mode is on' in result.output
-        assert 'Syncing' in result.output
+        result = runner.invoke(main, 
+                               ['--input-data', str(pkg_resources.resource_filename(resource_package, 'tests/test_data/test_data.csv')),
+                                '--output-dir', str(os.path.join(test_dir,'test_output')),
+                                '--num-runs', '3',
+                                '--pretrained', 'False',
+                                '--shiny-start', 'False'])
+        print(result)
+        self.assertEqual(result.exit_code, 0)
 
+
+    def test_CLI_fail(self):
+
+        return
 
 
 if __name__ == "__main__":
